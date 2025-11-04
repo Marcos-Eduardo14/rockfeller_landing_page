1e4e122b6604b7241989e9ab1af9c91e121769d8
daniel3243gg <daniew600@gmail.com>
2025-11-04 19:22:41 -0300

    Atualiza hero.js

--- assets/js/hero.js
+++ assets/js/hero.js
@@ -1,12 +1,15 @@
 export function heroHtml(hero) {
     const tagimg = document.createElement("img");
 
-    tagimg.src = gerarImagem(hero.mainImage.asset._ref)||"";
+    tagimg.src = gerarImagem(hero.mainImage[0].asset._ref)||"";
 
     return tagimg;  
 
-    function gerarImagem(img) {
+   
+}
+
+
+function gerarImagem(img) {
     const id = img.replace(/^image-/, '').replace(/-(\w+)$/, '.$1');
     return `https://cdn.sanity.io/images/gx7uk6s7/production/${id}`;
-    }
 }
